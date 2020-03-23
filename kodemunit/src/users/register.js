8bb2b2b3379a4606ffcb81c2ee0057dea7113c5b
benedict <bmsteve96@gmail.com>
2020-03-23 10:51:25 +0300

    basic styling of register page

--- kodemunit/src/users/register.js
+++ kodemunit/src/users/register.js
@@ -7,6 +7,7 @@ import { register } from "../redux/actions/auth";
 //conmponents
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Img from "../images/library.jpg";
 
 //styles
 import "../styles/users/style.css";
@@ -62,64 +63,74 @@ const Register = ({ register }) => {
   });
 
   return (
-    <>
-      <Header />
-      <main className="registration">
+    <div className='registration'>
+      <main>
         <div className='container'>
           <div className='user-registration'>
-            <header className='register'>
+            <div className='intro-image content'></div>
+            <header className='register content'>
               <h1>Register Now To Start your Journey with us</h1>
+              <p>
+                Fill this form to help us implement the best way to guide you in
+                your software development carrier.
+              </p>
             </header>
             <form onSubmit={e => handleSubmit(e)}>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <label htmlFor='first-name'>First Name</label>
-                <br />
-                <input
-                  id='first-name'
-                  placeholder='First Name'
-                  type='text'
-                  value={firstName}
-                  onChange={e => handleChange(e)}
-                  name='firstName'
-                  noValidate
-                />
+                <div className='input'>
+                  <input
+                    id='first-name'
+                    placeholder='First Name'
+                    type='text'
+                    value={firstName}
+                    onChange={e => handleChange(e)}
+                    name='firstName'
+                    noValidate
+                  />
+                </div>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <label htmlFor='last-name'>Last Name</label>
-                <br />
-                <input
-                  id='last-name'
-                  type='text'
-                  name='lastName'
-                  noValidate
-                  value={lastName}
-                  onChange={e => handleChange(e)}
-                  placeholder='Last Name'
-                />
+                <div className='input'>
+                  <input
+                    id='last-name'
+                    type='text'
+                    name='lastName'
+                    noValidate
+                    value={lastName}
+                    onChange={e => handleChange(e)}
+                    placeholder='Last Name'
+                  />
+                </div>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <label htmlFor='email'>Your email address</label>
-                <br />
-                <input
-                  type='email'
-                  name='email'
-                  id='email'
-                  value={email}
-                  onChange={e => handleChange(e)}
-                  placeholder='example@email.com'
-                />
+                <div className='input'>
+                  <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    value={email}
+                    onChange={e => handleChange(e)}
+                    placeholder='example@email.com'
+                  />
+                </div>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <label htmlFor='location'>Where are you from?</label>
-                <input
-                  id='location'
-                  name='locaTion'
-                  value={locaTion}
-                  onChange={e => handleChange(e)}
-                  placeholder='City, Country'
-                />
+                <div className='input'>
+                  <input
+                    type='text'
+                    id='location'
+                    name='locaTion'
+                    value={locaTion}
+                    onChange={e => handleChange(e)}
+                    placeholder='City, Country'
+                  />
+                </div>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <label htmlFor='comp-access'>
                   Do you own or have access to computer for coding?
                 </label>
@@ -135,7 +146,7 @@ const Register = ({ register }) => {
                   <option value='Always'>Always</option>
                 </select>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <p>
                   Kindly quantify the number of hours you’ll be able to dedicate
                   to the program every week
@@ -204,7 +215,7 @@ const Register = ({ register }) => {
                   6 PM - 8 PM{" "}
                 </label>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <p>
                   How many hours can you dedicate to the program in a week?{" "}
                   <span>(Choose one option)*</span>
@@ -262,7 +273,7 @@ const Register = ({ register }) => {
                   I can't quantify{" "}
                 </label>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <p>which learning ways/styles are good for you?</p>
                 <label htmlFor='visual'>
                   <input
@@ -334,19 +345,23 @@ const Register = ({ register }) => {
                   />{" "}
                   Logical-Theoretical{" "}
                 </label>
-                <a href='#'>Learn more about different learning style</a>
+                <a href='#' target='_blank' rel='noopener noreferrer'>
+                  Learn more about different learning style
+                </a>
               </div>
-              <div className='field-group'>
+              <div className='field-group content'>
                 <label htmlFor='about'>
                   tell us about yourself <span>(optional)*</span>
                 </label>
-                <textarea
-                  type='text'
-                  name='about'
-                  id='about'
-                  value={about}
-                  onChange={e => handleChange(e)}
-                />
+                <div className='input'>
+                  <textarea
+                    name='about'
+                    id='about'
+                    value={about}
+                    onChange={e => handleChange(e)}
+                    placeholder='About yourself'
+                  />
+                </div>
               </div>
               <div className='terms'>
                 <span>
@@ -360,7 +375,7 @@ const Register = ({ register }) => {
         </div>
       </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
@@ -373,6 +388,5 @@ const mapStateToProps = state => ({
 });
 
 export default connect(mapStateToProps, { register })(Register);
-
 
 //to add data in array i will have to create  a variable using react hooks and set it as an array...

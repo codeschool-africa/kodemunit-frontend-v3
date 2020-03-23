db6dd3501aaebbf424e5a930dd00b64d590c7236
benedict <bmsteve96@gmail.com>
2020-03-23 18:49:04 +0300

    updated register page

---
+++ kodemunit/src/components/data.js
@@ -0,0 +1,91 @@
+export const goodTimeCheckBoxes = [
+  {
+    value: "8 AM - 11 AM",
+    key: "eightam"
+  },
+  {
+    value: "11 AM - 1 PM",
+    key: "televenam'"
+  },
+  {
+    value: "1 PM - 4 PM",
+    key: "sevenpm"
+  },
+  {
+    value: "4 PM - 7 PM",
+    key: "fourpm"
+  },
+  {
+    value: "7 PM - 8 PM",
+    key: "sevenpm"
+  }
+];
+
+export const hoursWeekRadio = [
+  {
+    value: "1-3 hours",
+    key: "onetothree"
+  },
+  {
+    value: "4-6 hours",
+    key: "fourtosix"
+  },
+  {
+    value: "7-12 hours",
+    key: "seventotwelve"
+  },
+  {
+    value: "12+ hours",
+    key: "twelveplus"
+  },
+  {
+    value: "I can't quantify",
+    key: "cant-quantify"
+  }
+];
+
+export const learningStylesCheckBoxes = [
+  {
+    value: "Visual",
+    key: "visual"
+  },
+  {
+    value: "Auditory",
+    key: "auditory"
+  },
+  {
+    value: "Linguistic",
+    key: "linguistic"
+  },
+  {
+    value: "Practical",
+    key: "practical"
+  },
+  {
+    value: "Reflexive",
+    key: "reflexive"
+  },
+  {
+    value: "Social",
+    key: "social"
+  },
+  {
+    value: "Logical-Theoretical",
+    key: "logical"
+  }
+];
+
+export const CompAccessSelect = [
+    {
+        value: "Never"
+    },
+    {
+        value: "Sometimes"
+    },
+    {
+        value: "Often"
+    },
+    {
+        value: "Always"
+    }
+]
--- kodemunit/src/users/register.js
+++ kodemunit/src/users/register.js
@@ -4,10 +4,15 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { register } from "../redux/actions/auth";
 
-//conmponents
-import Header from "../components/header";
-import Footer from "../components/footer";
-import Img from "../images/library.jpg";
+//components
+import Footer from "../components/registerFooter";
+
+import {
+  learningStylesCheckBoxes,
+  goodTimeCheckBoxes,
+  hoursWeekRadio,
+  CompAccessSelect
+} from "../components/data";
 
 //styles
 import "../styles/users/style.css";
@@ -40,6 +45,8 @@ const Register = ({ register }) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
     console.log(formData);
   };
+
+  const handleCheckbox = () => {};
 
   const handleSubmit = async e => {
     e.preventDefault();
@@ -140,10 +147,13 @@ const Register = ({ register }) => {
                   onChange={e => handleChange(e)}
                 >
                   <option value=''>Please select</option>
-                  <option value='Never'>Never</option>
-                  <option value='Sometimes'>Sometimes</option>
-                  <option value='Often'>Often</option>
-                  <option value='Always'>Always</option>
+                  {CompAccessSelect.map(access => {
+                    return (
+                      <option value={access.value} key={access.value}>
+                        {access.value}
+                      </option>
+                    );
+                  })}
                 </select>
               </div>
               <div className='field-group content'>
@@ -151,200 +161,57 @@ const Register = ({ register }) => {
                   Kindly quantify the number of hours you’ll be able to dedicate
                   to the program every week
                 </p>
-                <label htmlFor='eightam'>
-                  <input
-                    type='checkbox'
-                    name='goodTime'
-                    id='eightam'
-                    value='8 AM - 10 AM'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  8 AM - 10 AM
-                </label>{" "}
-                <label htmlFor='tenam'>
-                  <input
-                    type='checkbox'
-                    name='goodTime'
-                    id='tenam'
-                    value='10 AM - 12 PM'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  10 AM - 12 PM{" "}
-                </label>
-                <label htmlFor='twelvepm'>
-                  <input
-                    type='checkbox'
-                    name='goodTime'
-                    id='twelvepm'
-                    value='12 PM - 2 PM'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  12 PM - 2 PM{" "}
-                </label>
-                <label htmlFor='twopm'>
-                  <input
-                    type='checkbox'
-                    name='goodTime'
-                    id='twopm'
-                    value='2 PM - 4 PM'
-                    onChange={e => handleChange(e)}
-                    autoComplete='new-password'
-                  />{" "}
-                  2 PM - 4 PM{" "}
-                </label>
-                <label htmlFor='fourpm'>
-                  <input
-                    type='checkbox'
-                    name='goodTime'
-                    id='fourpm'
-                    value='4 PM - 6 PM'
-                    onChange={e => handleChange(e)}
-                    autoComplete='new-password'
-                  />{" "}
-                  4 PM - 6 PM{" "}
-                </label>
-                <label htmlFor='sixpm'>
-                  <input
-                    type='checkbox'
-                    name='goodTime'
-                    id='sixpm'
-                    value='6 PM - 8 PM'
-                    onChange={e => handleChange(e)}
-                    autoComplete='new-password'
-                  />{" "}
-                  6 PM - 8 PM{" "}
-                </label>
+                {goodTimeCheckBoxes.map(access => {
+                  return (
+                    <label htmlFor={access.key} key={access.key}>
+                      <input
+                        type='checkbox'
+                        name='goodTime'
+                        id={access.key}
+                        value={access.value}
+                        onChange={e => handleChange(e)}
+                      />{" "}
+                      {access.value}
+                    </label>
+                  );
+                })}
               </div>
               <div className='field-group content'>
                 <p>
                   How many hours can you dedicate to the program in a week?{" "}
                   <span>(Choose one option)*</span>
                 </p>
-                <label htmlFor='onetothree'>
-                  <input
-                    type='radio'
-                    name='hoursWeek'
-                    id='onetothree'
-                    value='1-3 hours'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  1-3 hours
-                </label>{" "}
-                <label htmlFor='fourtosix'>
-                  <input
-                    type='radio'
-                    name='hoursWeek'
-                    id='fourtosix'
-                    value='4-6 hours'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  4-6 hours{" "}
-                </label>
-                <label htmlFor='seventotwelve'>
-                  <input
-                    type='radio'
-                    name='hoursWeek'
-                    id='seventotwelve'
-                    value='7-12 hours'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  7-12 hours{" "}
-                </label>
-                <label htmlFor='twelveplus'>
-                  <input
-                    type='radio'
-                    name='hoursWeek'
-                    id='twelveplus'
-                    value='12+ hours'
-                    onChange={e => handleChange(e)}
-                    autoComplete='new-password'
-                  />{" "}
-                  12+ hours{" "}
-                </label>
-                <label htmlFor='cant-quantify'>
-                  <input
-                    type='radio'
-                    name='hoursWeek'
-                    id='cant-quantify'
-                    value="I can't quantify"
-                    onChange={e => handleChange(e)}
-                    autoComplete='new-password'
-                  />{" "}
-                  I can't quantify{" "}
-                </label>
+                {hoursWeekRadio.map(access => {
+                  return (
+                    <label htmlFor={access.key} key={access.key}>
+                      <input
+                        type='radio'
+                        name='hoursWeek'
+                        id={access.key}
+                        value={access.value}
+                        onChange={e => handleChange(e)}
+                      />{" "}
+                      {access.value}
+                    </label>
+                  );
+                })}
               </div>
               <div className='field-group content'>
                 <p>which learning ways/styles are good for you?</p>
-                <label htmlFor='visual'>
-                  <input
-                    type='checkbox'
-                    name='learningStyle'
-                    id='visual'
-                    value='Visual'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  Visual
-                </label>{" "}
-                <label htmlFor='auditory'>
-                  <input
-                    type='checkbox'
-                    name='learningStyle'
-                    id='auditory'
-                    value='Auditory'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  Auditory{" "}
-                </label>
-                <label htmlFor='linguistic'>
-                  <input
-                    type='checkbox'
-                    name='learningStyle'
-                    id='linguistic'
-                    value='Linguistic'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  Linguistic{" "}
-                </label>
-                <label htmlFor='practical'>
-                  <input
-                    type='checkbox'
-                    name='learningStyle'
-                    id='practical'
-                    value='Practical'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  Practical{" "}
-                </label>
-                <label htmlFor='reflexive'>
-                  <input
-                    type='checkbox'
-                    name='learningStyle'
-                    id='reflexive'
-                    value='Reflexive'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  Reflexive{" "}
-                </label>
-                <label htmlFor='social'>
-                  <input
-                    type='checkbox'
-                    name='learningStyle'
-                    id='social'
-                    value='Social'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  Social{" "}
-                </label>
-                <label htmlFor='logical'>
-                  <input
-                    type='checkbox'
-                    name='learningStyle'
-                    id='logical'
-                    value='Logical-Theoretical'
-                    onChange={e => handleChange(e)}
-                  />{" "}
-                  Logical-Theoretical{" "}
-                </label>
+                {learningStylesCheckBoxes.map(access => {
+                  return (
+                    <label htmlFor={access.key} key={access.key}>
+                      <input
+                        type='checkbox'
+                        name='learningStyle'
+                        id={access.key}
+                        value={access.value}
+                        onChange={e => handleChange(e)}
+                      />{" "}
+                      {access.value}
+                    </label>
+                  );
+                })}
                 <a href='#' target='_blank' rel='noopener noreferrer'>
                   Learn more about different learning style
                 </a>
@@ -369,7 +236,7 @@ const Register = ({ register }) => {
                   <Link to='/term-and-conditions'>terms and conditions</Link>
                 </span>
               </div>
-              <button className="btn btn-secondary">Register</button>
+              <button className='btn btn-secondary'>Register</button>
             </form>
           </div>
         </div>

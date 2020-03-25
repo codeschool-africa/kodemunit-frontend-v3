485cf68a8e4d2e64055046a44111425863f944f2
benedict <bmsteve96@gmail.com>
2020-03-25 19:40:44 +0300

    updated team components

--- kodemunit/src/components/team.js
+++ kodemunit/src/components/team.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/all";
 import dp from "../images/profile.png";
+import { HashLink } from "react-router-hash-link";
 
 const Team = () => {
   return (
@@ -13,7 +14,39 @@ const Team = () => {
           <article>
             <img src={dp} alt='profile' />
             <h2>Justine Mahinyila</h2>
-            <h3>Co-Founder and Team Leader</h3>
+            <h3>Team Leader</h3>
+            <div className='social'>
+              <a href='#'>
+                <FaFacebookF className='icon' />
+              </a>
+              <a href='#'>
+                <FaTwitter className='icon' />
+              </a>
+              <a href='https://www.linkedin.com/in/justinemahinyila'>
+                <FaLinkedinIn className='icon' />
+              </a>
+            </div>
+          </article>
+          <article>
+            <img src={dp} alt='profile' />
+            <h2>Benedict Steven</h2>
+            <h3>Technical Lead (CTO)</h3>
+            <div className='social'>
+              <a href='https://www.facebook.com/profile.php?id=100006234616525'>
+                <FaFacebookF className='icon' />
+              </a>
+              <a href='http://www.twitter.com/bmsteven'>
+                <FaTwitter className='icon' />
+              </a>
+              <a href='https://www.linkedin.com/in/benedict-steven'>
+                <FaLinkedinIn className='icon' />
+              </a>
+            </div>
+          </article>
+          <article>
+            <img src={dp} alt='profile' />
+            <h2>Veronica Likunama</h2>
+            <h3>Community Leader</h3>
             <div className='social'>
               <a href='#'>
                 <FaFacebookF className='icon' />
@@ -28,19 +61,8 @@ const Team = () => {
           </article>
           <article>
             <img src={dp} alt='profile' />
-            <h2>Benedict Steven</h2>
-            <h3>Co-Founder and CTO</h3>
-            <div className='social'>
-              <a href='https://www.facebook.com/profile.php?id=100006234616525'>
-                <FaFacebookF className='icon' />
-              </a>
-              <a href='http://www.twitter.com/bmsteven'>
-                <FaTwitter className='icon' />
-              </a>
-              <a href='https://www.linkedin.com/in/benedict-steven'>
-                <FaLinkedinIn className='icon' />
-              </a>
-            </div>
+            <h2>This might be you</h2>
+            <h3><HashLink to="#contact">Join the Team</HashLink></h3>
           </article>
         </div>
       </div>
